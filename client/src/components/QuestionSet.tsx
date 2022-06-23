3756639eb7c87a980299d3af2aae7f7a7a8bf454
shahriar <shahriar.mehedi@bjitgroup.com>
2022-06-23 14:43:59 +0600

    next

--- client/src/components/QuestionSet.tsx
+++ client/src/components/QuestionSet.tsx
@@ -1,9 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import { Button, Modal } from "react-bootstrap";
 import axios from "axios";
 const QuestionSet: React.FC = () => {
+
+
+    //get All Question
+
+
+    const [bankData, setbankData] = useState<any[]>([]);
+
+    const getData = async() => {
+      await axios
+        .get("http://localhost:8000/api/question/getall")
+        .then((result) => {
+            setbankData(result.data.result);
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    };
+  
+    useEffect(() => {
+      getData();
+    }, []);
+
+
+    //get All questions
+
+
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -27,6 +53,7 @@ const QuestionSet: React.FC = () => {
     question_body: "",
     remarks: "",
     answer: "",
+    option:[]
   })
 
   const [optionDiv, setOptionDiv] = useState<any>([
@@ -46,13 +73,15 @@ const QuestionSet: React.FC = () => {
 
   const handleData = (e: any) => {
     const { name, value } = e.target;
-    setQuestionValue((val: any) => {
+    setValue((val: any) => {
       return {
         ...val,
         [name]: value,
       };
     });
   };
+
+const [questionArray,setQuestionArray]=useState<any>([])
 
   const handleQuestionData = (e: any) => {
     const { name, value } = e.target;
@@ -63,15 +92,61 @@ const QuestionSet: React.FC = () => {
       };
     });
   };
-  console.log("aa",postValue);
-  console.log("bb",optionValue);
+
+  const handleQuestionPush = ()=>{
+    // const isNonEmpty = !Object.values(questionValue).some(
+    //     (x) => x === null || x === ""
+    //   );
+      const newQuestion = {...questionValue}
+      newQuestion.options=optionValue
+          setQuestionArray([...questionArray,newQuestion])
+          setQuestionValue({
+            technology: "",
+            question_type: "",
+            job_rank: "",
+            difficulty: "",
+            question_body: "",
+            remarks: "",
+            answer: "",
+            option:[]
+          })  
+          handleClose() 
+  }
+console.log(questionArray);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const data: any = {
+      name: postValue.name,
+      deadline: postValue.deadline,
+      technology: postValue.technology,
+      job_rank: postValue.job_rank,
+      difficulty: postValue.difficulty,
+      questions: questionArray,
+    };
+ 
+    const createTable = async () => {
+      await axios.post("http://localhost:8000/api/set/post", data);
+    };
+    createTable();
+    alert("Inserted");
+    navigate("/home");
+  };
+
+
+  const handleQuestionBank=(question:any)=>{
+      const {_id,...rest}=question
+      const newObject={...rest,bank_id:_id}
+        questionArray.push(newObject)     
+  }
+  console.log(questionArray);
   
   return (
     <>
       <div className="container">
         <Navbar />
         <NavLink to="/">home</NavLink>
-        <form className="mt-4" id="myform">
+        <form className="mt-4  col-md-6" id="myform" onSubmit={handleSubmit}>
           <div className="row">
             <div className="mb-3 col-lg-6 col-md-6 col-12">
               <label className="form-label">Set Name</label>
@@ -95,7 +170,7 @@ const QuestionSet: React.FC = () => {
               <label className="form-label">Technology</label>
               <input
                 type="text"
-                name="job_rank"
+                name="technology"
                 className="form-control"
                 onChange={handleData}
               />
@@ -122,7 +197,7 @@ const QuestionSet: React.FC = () => {
             <Button className="btn btn-primary" onClick={handleShow}>
               Add Questions
             </Button>
-            <Modal show={show} onHide={handleClose}>
+            <Modal show={show} onHide={handleClose} >
               <Modal.Header closeButton>
                 <Modal.Title>Questions</Modal.Title>
               </Modal.Header>
@@ -205,7 +280,7 @@ const QuestionSet: React.FC = () => {
                 />
               </div>
             ))}
-            <p className="btn btn-primary" onClick={handleClick}>
+            <p className="btn btn-primary" onClick={handleClick} >
               Add Options
             </p>
           </div>
@@ -214,7 +289,7 @@ const QuestionSet: React.FC = () => {
                 <Button variant="secondary" onClick={handleClose}>
                   Close
                 </Button>
-                <Button variant="primary" onClick={handleClose}>
+                <Button variant="primary" type="submit" onClick={handleQuestionPush}>
                   Save Changes
                 </Button>
               </Modal.Footer>
@@ -226,6 +301,41 @@ const QuestionSet: React.FC = () => {
               Submit
             </button>
         </form>
+
+<br />
+<br />
+        <div >
+            <h3>All Questions</h3>
+            <div className="mt-5">
+        <div className="container">
+          <table className="table">
+            <thead>
+              <tr className="table-dark">
+                <th scope="col">id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {bankData &&
+                bankData.map((element, id) => {
+                  return (
+                    <tr key={id}>
+                      <th scope="row">{id + 1}</th>
+                      <td>{element.question_body}</td>
+                      <td className="d-flex justify-content-between">
+                        <button className="btn btn-danger" onClick={()=>handleQuestionBank(element)}>
+                          Add
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+      </div>  
+        </div>
       </div>
     </>
   );
